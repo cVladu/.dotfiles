06b852d6de2aa74b8124752a02420afe088db1e1
cVladu <catalin.alexandru.vladu@gmail.com>
2024-03-08 20:28:51 +0200

    Changed the way branches are displyated by using telescope instead of fugitive

--- nvim/lua/user/plugins/fugitive.lua
+++ nvim/lua/user/plugins/fugitive.lua
@@ -2,7 +2,6 @@ return {
   "https://github.com/tpope/vim-fugitive",
   keys = {
     { "<leader>gg", "<cmd>:Git<cr>",         desc = "fugitive: Open git status" },
-    { "<leader>gb", "<cmd>:Git branch<cr>",  desc = "fugitive: Open git branch" },
     { "<leader>gB", "<cmd>:Git blame<cr>",   desc = "fugitive: Open git blame" },
     { "<leader>gc", "<cmd>:Git commit<cr>",  desc = "fugitive: Open git commit" },
     { "<leader>gd", "<cmd>:Gdiffsplit<cr>", desc = "fugitive: Open git diff" },
