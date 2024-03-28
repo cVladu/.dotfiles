60453e7c005851ada264ce77e2a8394e49a443aa
cVladu <catalin.alexandru.vladu@gmail.com>
2024-03-28 16:07:45 +0200

    Modified lua line to include the full path of the file.

--- nvim/lua/user/plugins/colorscheme.lua
+++ nvim/lua/user/plugins/colorscheme.lua
@@ -5,6 +5,14 @@ return {
       require('lualine').setup {
         options = {
           theme = "nightfly",
+        },
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1
+            }
+          },
         }
       }
     end
