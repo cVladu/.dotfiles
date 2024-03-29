a1e4e21b33294ca3ab51af4926d709c5c781921b
cVladu <catalin.alexandru.vladu@gmail.com>
2024-03-29 10:37:13 +0200

    Added the new dependency for nvim-dap

--- nvim/lua/user/plugins/debug.lua
+++ nvim/lua/user/plugins/debug.lua
@@ -3,7 +3,8 @@ return {
   {
     "rcarriga/nvim-dap-ui",
     dependencies = {
-      "mfussenegger/nvim-dap"
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
     },
     config = function()
       local dapui = require('dapui')
